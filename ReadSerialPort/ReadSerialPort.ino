20721cffb3ad963476daeaba1e5a5a3a4b7276d8
Michael Macewich <maceww6438@gmail.com>
2020-10-31 09:55:34 -0500

    Refactor for simple

--- ReadSerialPort/ReadSerialPort.ino
+++ ReadSerialPort/ReadSerialPort.ino
@@ -1,5 +1,8 @@
 void setup() {
   Serial.begin(9600);
+  int reading = analogRead(A0);
+  Serial.println(0);
+  Serial.println(reading);
 }
 
 int reading = 0;
@@ -12,27 +15,23 @@ int zeros = 0;
 
 // the loop routine runs over and over again forever:
 void loop() {
-  i = 1;
-  for(uint32_t tStart = millis();  (millis()-tStart) < period;  ) {
-    count++;
-    reading = analogRead(A0);
-    if (reading < 1) {
-      zeros++;
-    if (zeros >= 3) {
-      zeros = 0;
-      total = 0;
-      count = 1;
-//      Serial.println("The blower shut off");
-      delay(30000);
-      break;
-    }
-    }
-    total += reading;
+  count = 0;
+  total = 0;
+  reading = analogRead(A0);
+  while (reading == 0) {
     delay(500);
-    i++;
+    reading = analogRead(A0);
+  }
+  for(uint32_t tStart = millis();  (millis()-tStart) < period;  ) {
+    reading = analogRead(A0);
+    if (reading == 0) {
+      Serial.println(reading);
+      break;
+      }
+    total += reading;
+    count++;
+    delay(500);
   }
   average = total / count;
   Serial.println(average);
-  count = 0;
-  total = 0;
 }
