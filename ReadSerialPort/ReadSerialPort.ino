0663a25407659fe85bd6717fa01c0057e706594e
Michael Macewich <maceww6438@gmail.com>
2020-11-02 07:02:53 -0600

    Tighten up transitions

--- ReadSerialPort/ReadSerialPort.ino
+++ ReadSerialPort/ReadSerialPort.ino
@@ -1,8 +1,13 @@
+void putString(String s) {
+//  Serial.print(s);
+}
+
 void setup() {
   Serial.begin(9600);
+  putString("Start tracking...");
   int reading = analogRead(A0);
-  Serial.println(0);
-  Serial.println(reading);
+  putString("First SEND and first reading...");
+  Serial.println(reading,1);
 }
 
 int reading = 0;
@@ -11,27 +16,41 @@ int count = 0;
 float average = 0.0;
 uint32_t period = 60000L;       // 1 minute
 int i;
-int zeros = 0;
+bool zero = false;
 
 // the loop routine runs over and over again forever:
 void loop() {
   count = 0;
   total = 0;
   reading = analogRead(A0);
+  if (reading == 0) {
+    Serial.println(0.0,4);
+  }
+  putString("Check for string of zeros");
   while (reading == 0) {
     delay(500);
     reading = analogRead(A0);
   }
+  putString("No zeros");
+  total += reading;
+  count++;
   for(uint32_t tStart = millis();  (millis()-tStart) < period;  ) {
     reading = analogRead(A0);
     if (reading == 0) {
-      Serial.println(reading);
+      putString("Encountered zero during 1 minute loop");
+      zero = true;
       break;
       }
     total += reading;
     count++;
     delay(500);
   }
+  putString("SEND average: ");
   average = total / count;
-  Serial.println(average);
+  Serial.println(average,3);
+  if (zero) {
+    delay(1050);
+    Serial.println(0.0,2);
+    zero = false;
+  }
 }
