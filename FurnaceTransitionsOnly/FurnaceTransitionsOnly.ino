d07866c9f63773969d3c5f89b9324bd9aa2e4673
Michael Macewich <maceww6438@gmail.com>
2020-11-03 20:11:02 -0600

    Send transitions and timeouts only

--- FurnaceTransitionsOnly/FurnaceTransitionsOnly.ino
+++ FurnaceTransitionsOnly/FurnaceTransitionsOnly.ino
@@ -4,11 +4,19 @@ void putString(String s) {
 
 bool waitForZero = false;
 int reading = 0;
+int count = 0;
+int total = 0;
+uint32_t expired;
 
 void setup() {
   Serial.begin(9600);
   putString("Start tracking...");
   putString("Initial reading...");
+  // Calm down the signal
+  for (int i=0; i<4; i++) {
+    reading = analogRead(A0);
+    delay(250);  
+  }
   reading = analogRead(A0);
   Serial.println(reading, 5);
   if (reading == 0) {
@@ -19,16 +27,31 @@ void setup() {
   }
 }
 
+uint32_t period = 60000L; 
+float average;
 
 // the loop routine runs over and over again forever:
 void loop() {
   if (waitForZero) {
+    uint32_t tStart = millis();
+    count = 0;
+    total = 0;
     reading = analogRead(A0);
     while (reading != 0) {
+      count++;
+      total += reading;
       delay(250);
-      reading = analogRead(A0);    
+      expired = millis() - tStart;
+      if (expired > period) {
+        average = total / count;
+        Serial.println(average,1);
+        count = 0;
+        total = 0;
+        tStart = millis();
+      }
+      reading = analogRead(A0);
     }
-    Serial.println(reading, 4);
+    Serial.println(0.0, 4);
     waitForZero = false;
   }
 
@@ -38,7 +61,7 @@ void loop() {
       delay(250);
       reading = analogRead(A0);
     }
-    Serial.println(reading, 3);
+    Serial.println(32.0, 3);
     waitForZero = true;
   }
 }
