19b995fd68e885e0b26ff727244815932d80381e
Mike Macewich <maceww6438@gmail.com>
2022-03-19 20:14:06 -0500

    Added tryzub data, Ukie flag,

--- nanoScrollingBitmap/nanoScrollingBitmap.ino
+++ nanoScrollingBitmap/nanoScrollingBitmap.ino
@@ -3,16 +3,12 @@
 #define FASTLED_INTERNAL
 #include <FastLED.h>
 
-// Message parameters
-#define MESSAGE_WIDTH 80 // The number of columns in the messasge This should be calculable
-#define LETTER_HEIGHT 12 // Number of rows in each letter. Possible less than MATRIX_HEIGHT
-
 // LED Matrix parameters
 #define MATRIX_HEIGHT 16
 #define MATRIX_WIDTH  16
 #define NUM_LEDS      MATRIX_HEIGHT * MATRIX_WIDTH
 
-#define BITMAP_WIDTH  160
+// #define BITMAP_WIDTH  160
 #define DATA_PIN 3
 #define CLOCK_PIN 13
 #define PAUSE 10
@@ -21,13 +17,10 @@
 #define MAX_mAMPS 500
 
 #define SPEED 100
-#define printMatrix true
+#define DEBUG_PRINT false
 
 CRGB leds[NUM_LEDS];
 CRGB color = CRGB::White;
-
-int lastMessageColumn = MESSAGE_WIDTH - 1;
-int currentMessageColumn = 0;
 
 // These are the LED numbers for the last column of the matrix
 int ledsInLastColumn[] = {
@@ -35,6 +28,41 @@ int ledsInLastColumn[] = {
   96, 127, 128, 159, 160,
   191, 192, 223, 224, 255
   };
+
+// This is a degenerate form of showing the flag
+// without having to do a multicolored bitmap design
+void ukrainianFlag() {
+  FastLED.clear();
+  int topHalf = 256 / 2;
+  for (int i = 32; i < topHalf; i++) {
+    leds[i] = CRGB::Blue;
+  }
+  for (int i = topHalf; i < 256 - 32; i++) {
+    leds[i] = CRGB::Yellow;
+  }
+  FastLED.show();
+}
+
+// bitmap design for ukrainian tryzub
+uint16_t tryzub[16] = {
+  0b0000000100000000,
+  0b0000000100000000,
+  0b0010001110001000,
+  0b0011000100011000,
+  0b0010100100101000,
+  0b0010101110101000,
+  0b0011001010011000,
+  0b0011101010111000,
+  0b0010110101101000,
+  0b0010010101001000,
+  0b0011111111111000,
+  0b0000010001000000,
+  0b0000011011000000,
+  0b0000000100000000,
+  0b0000000100000000,
+  0b0000000100000000
+};
+
 
 uint16_t slava[] = {
 0x0FE0,
@@ -198,14 +226,27 @@ uint16_t slava[] = {
 0x0000,
 0x0000
 };
+int MESSAGE_WIDTH = sizeof(slava) / sizeof(uint16_t);
+int currentMessageColumn = 0;
 
 // print bitmap from memory
 void printBitmap(uint16_t bitmap[], CRGB myColor) {
   Serial.println("Bitmap to follow:");
-  Serial.println("fedcba9876543210 xxxx fedcba9876543210xxxx");
-  for (int row = 0; row < BITMAP_WIDTH; row++) {
-  Serial.print(bitmap[row], BIN); Serial.print(" ");
-  Serial.print(bitmap[row], HEX); Serial.print(" ");
+  Serial.println("xxxx  fedcba9876543210 fedcba9876543210");
+
+  // Print the symbols
+  for (int row = 0; row < MESSAGE_WIDTH; row++) {
+    // Print the hex
+    char hexVal[4];
+    sprintf(hexVal, "%04X ", bitmap[row]);
+    Serial.print(hexVal); Serial.print(" ");
+    
+    // Print the binary
+    for (unsigned int test = 0x8000; test; test >>= 1) {
+      Serial.print(bitmap[row] & test ? '1' : '0');
+    }
+    Serial.print(" ");
+    
     for (int col = 15; col >=0; col--) {
       if (bitRead(bitmap[row], col)) {
         Serial.print("x");
@@ -222,7 +263,7 @@ void getNextBitarrayRow() {
   for (int row = 0; row < MATRIX_HEIGHT; row++) {
     leds[ledsInLastColumn[row]] = CRGB::Black;
   }
-//  Serial.println(slava[currentMessageColumn], BIN);
+
   // For each bit that's 1, light up the LED
   for (int b = 0; b <16; b++) {
     if (bitRead(slava[currentMessageColumn], b) == 1) {
@@ -246,19 +287,18 @@ void scrollMatrixLeft(int scrollDelay) {
       }
     }
 
-    //    getNextMessageRow();  // Scroll the message using the bit matrix
     getNextBitarrayRow(); // Scroll the message using the bit array
 
     FastLED.show();
     delay(scrollDelay);
-    if (printMatrix) {
+    if (DEBUG_PRINT) {
       printLEDMatrix();
     }
   }
 }
 
 void printLEDMatrix() {
-//  Serial.println("print LED matrix");
+//  Serial.println("print contents of LED matrix");
   int x = MATRIX_WIDTH - 1; // index of leftmost pixel in the 0th row
   int y = ((MATRIX_WIDTH - 1) * MATRIX_WIDTH) - 1; // index of pixel to the left of the rightmost
   for (int start = x; start <= y; start += MATRIX_WIDTH * 2) {
@@ -285,6 +325,7 @@ void printLEDMatrix() {
 }
 
 // Convert from (0,0) in upper left to matrix index
+// Used when turning on an individual (random) pixel
 int mapScreenToMatrix(int row, int col) {
   // Switch from increasing left to right into
   // increasing right to left for even numbered rows
@@ -303,13 +344,14 @@ void setup() {
   FastLED.clear();
   FastLED.show();
 
-  // This is the message that will scroll
-  // Adjust the message parameter defines
-  // (These parameters should be encapsulated)
-  //  slavaUkraine(); // This is the message that will scroll
-  printBitmap(slava, CRGB::White);
+  if (DEBUG_PRINT) {
+    printBitmap(slava, CRGB::White);
+    printBitmap(tryzub, CRGB::Blue);
+  }
 }
 
 void loop() {
+  ukrainianFlag();
+  delay(3000);
   scrollMatrixLeft(SPEED);
 }
