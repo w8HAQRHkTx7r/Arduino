6bffdb274a9a22b9489035f2b2d59cc8937c1290
Mike Macewich <maceww6438@gmail.com>
2022-04-16 06:36:18 -0500

    Minimum Viabla Product. The wakes up when the reed switch is separated and begins transmitting. When the reed switch is brought back together, the TX goes back to sleep. modified:   esp32MailboxSensorTX/esp32MailboxSensorTX.ino new file:   esp32MailboxSensorTXSleep/esp32MailboxSensorTXSleep.ino

--- esp32MailboxSensorTXSleep/esp32MailboxSensorTXSleep.ino
+++ esp32MailboxSensorTXSleep/esp32MailboxSensorTXSleep.ino
@@ -2,7 +2,7 @@
   ESP-NOW Demo - Transmit
   esp-now-demo-xmit.ino
   Sends data to Responder
-  
+
   DroneBot Workshop 2022
   https://dronebotworkshop.com
 */
@@ -22,6 +22,7 @@ uint8_t broadcastAddress[] = {0x94, 0xB9, 0x7E, 0x79, 0xC8, 0x28};
 
 // Define a data structure
 typedef struct struct_message {
+  //  char a[32];
   unsigned long tymeStamp;
   bool door;
   int bootCount;
@@ -34,8 +35,26 @@ struct_message myData;
 esp_now_peer_info_t peerInfo;
 
 void printBroadcastAddress() {
-  for (int hexade=0; hexade<6; hexade++) {
-    Serial.print(broadcastAddress[hexade], HEX); if (hexade < 5) {Serial.print(":");}  
+  for (int hexade = 0; hexade < 6; hexade++) {
+    Serial.print(broadcastAddress[hexade], HEX); if (hexade < 5) {
+      Serial.print(":");
+    }
+  }
+}
+
+void print_wakeup_reason() {
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch (wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n", wakeup_reason); break;
   }
 }
 
@@ -51,10 +70,17 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
 }
 
 void setup() {
-  
+
   // Set up Serial Monitor
   Serial.begin(115200);
-  pinMode(13, OUTPUT);       // External confirmation 
+  while (!Serial) {
+    ;
+  }
+  //Increment boot number and print it every reboot
+  ++bootCount;
+  Serial.println("Boot number: " + String(bootCount));
+
+  pinMode(13, OUTPUT);       // External confirmation
   pinMode(26, INPUT_PULLUP); // Reed switch RTC pin number
   digitalWrite(13, LOW);     // Turn LED off
 
@@ -62,6 +88,8 @@ void setup() {
   WiFi.mode(WIFI_STA);
   Serial.print("I'm TX MAC Address: ");
   Serial.println(WiFi.macAddress());
+
+  print_wakeup_reason();
 
   // Initilize ESP-NOW
   if (esp_now_init() != ESP_OK) {
@@ -71,49 +99,71 @@ void setup() {
 
   // Register the send callback
   esp_now_register_send_cb(OnDataSent);
-  
+
   // Register peer
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;  
+  peerInfo.channel = 0;
   peerInfo.encrypt = false;
-  
-  // Add peer        
-  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+
+  // Add peer
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
     Serial.println("Failed to add peer");
     return;
   }
 }
 
 void loop() {
+  Serial.print(millis());
+  Serial.print(" Loop ");
+  
   int door = digitalRead(26); // The switch must already have opened, because this woke up
-                              // So what's the point of reading the current state?
+  Serial.print("Door state is ");
+  Serial.println(door);
+  
+  // So what's the point of reading the current state?
   // Create test data
   // Format structured data
   myData.door = door;
   myData.tymeStamp = millis();
   myData.bootCount = bootCount;
-  
+
   // Send message via ESP-NOW
   digitalWrite(13, HIGH); // Flash the LED to indicate this code is running
   esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
-   
+
   if (result == ESP_OK) {
     Serial.print("esp_now_send() OK ");
+    delay(500);
+    digitalWrite(13, LOW);
+    delay(2000);
+    esp_sleep_enable_ext0_wakeup(GPIO_NUM_26, 1); //1 = High, 0 = Low
+
+    //If you were to use ext1, you would use it like
+    //esp_sleep_enable_ext1_wakeup(BUTTON_PIN_BITMASK,ESP_EXT1_WAKEUP_ANY_HIGH);
+
+    //Go to sleep now
+    Serial.println("Going to sleep now");
+    esp_deep_sleep_start();
+    Serial.println("This will never be printed");
   }
   else {
     Serial.print("esp_now_send() not OK ");
+    delay(500);
+    digitalWrite(13, LOW);
+    delay(2000);
   }
-  delay(500);
-  digitalWrite(13, LOW);
-  delay(2000);
-//  esp_sleep_enable_ext0_wakeup(GPIO_NUM_33,1); //1 = High, 0 = Low
-  // Won't this keep triggering over and over again because right after it goes to sleep
-  // the door is in the open state and will therefore wake up.
+  //  delay(500);
+  //  digitalWrite(13, LOW);
+  //  delay(2000);
+  //  esp_sleep_enable_ext0_wakeup(GPIO_NUM_33,1); //1 = High, 0 = Low
+  //
+  //  //If you were to use ext1, you would use it like
+  //  //esp_sleep_enable_ext1_wakeup(BUTTON_PIN_BITMASK,ESP_EXT1_WAKEUP_ANY_HIGH);
+  //
+  //  //Go to sleep now
+  //  Serial.println("Going to sleep now");
+  //  esp_deep_sleep_start();
+  //  Serial.println("This will never be printed");
+  //
 
-  //If you were to use ext1, you would use it like
-  //esp_sleep_enable_ext1_wakeup(BUTTON_PIN_BITMASK,ESP_EXT1_WAKEUP_ANY_HIGH);
-
-  //Go to sleep now
-//  Serial.println("Going to sleep now");
-//  esp_deep_sleep_start();
 }
